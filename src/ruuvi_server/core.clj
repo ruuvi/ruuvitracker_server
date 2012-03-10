a182a0468a10e993802343176a8b52998c82c973
Juha Syrjälä <juha.syrjala@gmail.com>
2012-03-10 19:35:32 +0200

    .html suffix is not required, files without suffix are assumed to be html files. Also fixed bug in index.html handling.

--- src/ruuvi_server/core.clj
+++ src/ruuvi_server/core.clj
@@ -1,17 +1,12 @@
 (ns ruuvi-server.core
   (:require [ruuvi-server.api :as api])
   (:use ruuvi-server.common)
-  (:use ruuvi-server.database)
-  (:require [ruuvi-server.models.entities :as db])
   (:use compojure.core)
   (:require [compojure.route :as route]
             [compojure.handler :as handler])
   (:use ring.adapter.jetty)
   (:use ring.middleware.reload)
   (:use ring.middleware.stacktrace)
-  (:use ring.middleware.json-params)
-  (:use ring.middleware.params)
-  (:use ring.middleware.session)
   (:use [clojure.tools.logging :only (debug info warn error)])
   )
 
@@ -23,18 +18,30 @@
        "<ul>"
        ))
 
+(defn- wrap-add-html-suffix
+  "Adds .html URI:s without dots and without / ending"
+  [handler]
+  (fn [req]
+    (handler
+     (update-in req [:uri]
+                #(if (and (not (.endsWith % "/")) (< (.indexOf % ".") 0))
+                   (str % ".html")
+                   %)))))
+
 (defn- wrap-dir-index
   "Convert paths ending in / to /index.html"
   [handler]
   (fn [req]
     (handler
      (update-in req [:uri]
-                #(if (.endsWith "/" %) "/index.html" %)))))
+                #(if (.endsWith % "/" )
+                   (str % "index.html")
+                   %)))))
 
 (defroutes main-routes
   (GET "/api" [] api-doc-response)
   (context "/api" [] api/api-routes)
-  (wrap-dir-index (route/resources "/"))
+  (wrap-dir-index (wrap-add-html-suffix (route/resources "/")))
   (route/not-found "<h1>not found</h1>")
   )
 
