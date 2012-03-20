7291d5e03073cebd4c47dc4fcf50d60e2d501d1e
Juha Syrjälä <juha.syrjala@gmail.com>
2012-03-20 09:17:58 +0200

    Changed date functions to use UTC timezone. Fixes util.clj tests

--- src/ruuvi_server/util.clj
+++ src/ruuvi_server/util.clj
@@ -1,7 +1,12 @@
 (ns ruuvi-server.util
     (:import [org.joda.time.format DateTimeFormat DateTimeFormatter]
-           [org.joda.time DateTime])
-)
+             [org.joda.time DateTime DateTimeZone]
+             ))
 
-(def date-time-formatter (DateTimeFormat/forPattern "YYYY-MM-dd'T'HH:mm:ss.SSSZ"))
+
+(def date-time-formatter (.withZone
+                          (DateTimeFormat/forPattern "YYYY-MM-dd'T'HH:mm:ss.SSSZ")
+                          (DateTimeZone/forID "UTC")
+  ))
+
 (defn timestamp [] (.print date-time-formatter (new org.joda.time.DateTime)))
--- test/ruuvi_server/test/util.clj
+++ test/ruuvi_server/test/util.clj
@@ -2,28 +2,23 @@
   (:import [org.joda.time DateTime DateTimeUtils])
   (:use ruuvi-server.util)
   (:use midje.sweet)
-  (:import java.util.TimeZone)
+  (:import org.joda.time.DateTimeZone)
   )
 
-(def date-time (DateTime. 2012, 3, 18, 23, 25, 9,213))
-(def date-time-text "2012-03-18T23:25:09.213+0200")
-;; TODO currently util.clj uses default timezone, maybe it should use UTC always
-(let [default-timezone (TimeZone/getDefault)]
-  (try
-    (TimeZone/setDefault (TimeZone/getTimeZone "Helsinki/Finland"))
+(def local-timezone (DateTimeZone/forID "Europe/Helsinki"))
+(def utc-timezone (DateTimeZone/forID "UTC"))
+(def local-date-time (DateTime. 2012, 3, 18, 23, 25, 9,213, local-timezone))
+(def parsed-date-time (DateTime. 2012, 3, 18, 21, 25, 9,213, utc-timezone))
+(def date-time-text "2012-03-18T21:25:09.213+0000")
 
-    (fact "date-time-formatter formats a DateTime object as a string"
-          (.print date-time-formatter date-time) => date-time-text)
-    
-    (fact "date-time-formatter parses a string to DateTime object"
-          (.parseDateTime date-time-formatter date-time-text) => date-time)
+(fact "date-time-formatter formats a DateTime object as a string"
+      (.print date-time-formatter local-date-time) => date-time-text)
 
-    (try
-      (DateTimeUtils/setCurrentMillisFixed  (.getMillis date-time))
-      (fact "timestamp returns current time formatted as a string"
-            (timestamp) => date-time-text)  
-      (finally (DateTimeUtils/setCurrentMillisSystem )))
-    
-    (finally
-     (TimeZone/setDefault default-timezone)
-   )))
+(fact "date-time-formatter parses a string to DateTime object"
+      (.parseDateTime date-time-formatter date-time-text) => parsed-date-time)
+
+(try
+  (DateTimeUtils/setCurrentMillisFixed  (.getMillis local-date-time))
+  (fact "timestamp returns current time formatted as a string"
+        (timestamp) => date-time-text)  
+  (finally (DateTimeUtils/setCurrentMillisSystem )))
