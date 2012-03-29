318fcd4e6c87630b8ba87375024668dccaa2614e
Juha Syrjälä <juha.syrjala@gmail.com>
2012-03-30 00:04:30 +0300

    ruuviserver.standalone.starter can now start server in prod mode. Added dev/prod command line parameter to starter.

--- src/ruuvi_server/standalone/starter.clj
+++ src/ruuvi_server/standalone/starter.clj
@@ -2,12 +2,18 @@
   (:use ruuvi-server.core)
   (:use ruuvi-server.standalone.config)
   )
-
+;; USAGE:
+;;   lein run -m ruuvi-server.standalone.starter prod
+;;   lein run -m ruuvi-server.standalone.starter dev
 ;; standalone webapp starter
-(defn -main []
+(defn -main [arg]
   ;; TODO this executes start-dev
   ;; should only execute start-dev if some environment variable is set
   (init-config)
-  (start-dev {:server-port *server-port*
-              :database-config *database-config*})
+  (if (= arg "prod")
+    (start-prod {:server-port *server-port*
+                :database-config *database-config*})
+    (start-dev {:server-port *server-port*
+                :database-config *database-config*})
+    )
   )
