a48f75d4a06d8fab619da1588f120e6739cb929d
Juha Syrjälä <juha.syrjala@gmail.com>
2012-04-03 19:58:36 +0300

    Implemented HMAC-SHA1 algorithms for tracker api authentication. Actual authentication still used old SHA1 method.

--- src/ruuvi_server/tracker_security.clj
+++ src/ruuvi_server/tracker_security.clj
@@ -1,32 +1,46 @@
 (ns ruuvi-server.tracker-security
   (:import [java.security MessageDigest])
+  (:import [javax.crypto Mac])
+  (:import [javax.crypto.spec SecretKeySpec])
   (:import [org.apache.commons.codec.binary Hex])
-  (:use [clojure.tools.logging :only (debug info warn error)])
-  )
+  (:use [clojure.tools.logging :only (debug info warn error)]))
 
-(defn- compute-mac [params secret mac-field]
-  (let [request-mac (params mac-field)
-        ; sort keys alphabetically
-        sorted-keys (sort (keys params))
-        ; remove mac key
-        included-keys (filter (fn [param-key]
-                                (not= mac-field param-key))
-                              sorted-keys)
-        ; make included-keys a vector and convert to non-lazy list
-        param-keys (vec included-keys)]
+(defn generate-mac-message [params mac-field]
+  (let [;; remove mac key
+        non-mac-params (dissoc params mac-field)
+        ;; sort keys alphabetically
+        sorted-keys (sort (keys non-mac-params))
+        ;; make included-keys a vector and convert to non-lazy list
+        param-keys (vec sorted-keys)]
+    ;; concatenate keys, values and separators
+    (apply str (for [k param-keys]
+                 (str (name k) ":" (params k) "|")))))
+  
+(defn compute-mac [params secret mac-field]
+  (let [mac-message (generate-mac-message params mac-field)
+        request-mac (params mac-field)]
     
-   ; concatenate keys, values and separators. also add shared secret
-   (let [value (str (apply str (for [k param-keys]
-                                  (str (name k) ":" (params k) "|")
-                                  )))
-         value-with-shared-secret (str value secret)
-         messageDigester (MessageDigest/getInstance "SHA-1")]
+    (let [value-with-shared-secret (str mac-message secret)
+          messageDigester (MessageDigest/getInstance "SHA-1")]
       (let [computed-mac (.digest messageDigester (.getBytes value-with-shared-secret "ASCII"))
             computed-mac-hex (Hex/encodeHexString computed-mac)]
         (debug (str "orig-mac "(str request-mac) " computed mac " (str computed-mac-hex)) )
         computed-mac-hex
         ))))
 
+(defn compute-hmac [params secret mac-field]
+  (let [mac-message (generate-mac-message params mac-field)
+        request-mac (params mac-field)
+        algorithm "HmacSHA1"
+        mac (Mac/getInstance algorithm)
+        secret-key (SecretKeySpec. (.getBytes secret "ASCII") algorithm)]
+    
+    (.init mac secret-key)
+    (let [computed-hmac (.doFinal mac (.getBytes mac-message "ASCII"))
+          computed-hmac-hex (Hex/encodeHexString computed-hmac)]
+      (debug (str "orig-hmac "(str request-mac) " computed hmac " (str computed-hmac-hex)) )
+      computed-hmac-hex
+      )))
 
 (defn authentication-status
 "Sets keys
--- test/ruuvi_server/test/tracker_security.clj
+++ test/ruuvi_server/test/tracker_security.clj
@@ -3,28 +3,51 @@
   (:use ruuvi-server.tracker-security)
   )
 
+(def valid-mac "17e4ccf60f766710d0695348d7fda63cee0a3d46")
 (def valid-params {:X-foobar "bar",
                    :tracker_code "foobar",
                    :longitude "02536.100084,E",
-                   :mac "17e4ccf60f766710d0695348d7fda63cee0a3d46",
+                   :mac valid-mac
                    :version "1",
                    :time "2012-04-02T18:35:11.000+0200",
                    :latitude "6457.934248,N"})
 
-(def valid-tracker {:shared_secret "foobar"})
+(def valid-shared-secret "foobar")
+(def valid-tracker {:shared_secret valid-shared-secret})
   
-(fact
+(fact "when tracker is not available, authentication is not attempted"
  (authentication-status valid-params nil :mac )
  => {:unknown-tracker true})
 
-(fact
+(fact "when mac field is not in message, authentication is not used"
  (authentication-status {:some "fields"} valid-tracker :mac)
  => {:not-authenticated true})
 
-(fact
+(fact "when computed mac does not match mac in message, authentication fails"
  (authentication-status valid-params {:shared_secret "wrong-shared-secret"} :mac)
  => {:authentication-failed true})
 
-(fact
+(fact "when computed mac matches mac in message, authentication succeeds"
  (authentication-status valid-params valid-tracker :mac)
- => {:authenticated-tracker true})
+ => {:authenticated-tracker true}
+ (provided (compute-mac valid-params valid-shared-secret :mac) => valid-mac))
+
+
+(fact
+ (generate-mac-message valid-params :mac) => "X-foobar:bar|latitude:6457.934248,N|longitude:02536.100084,E|time:2012-04-02T18:35:11.000+0200|tracker_code:foobar|version:1|")
+
+(fact
+ (generate-mac-message valid-params :not-exists) => "X-foobar:bar|latitude:6457.934248,N|longitude:02536.100084,E|mac:17e4ccf60f766710d0695348d7fda63cee0a3d46|time:2012-04-02T18:35:11.000+0200|tracker_code:foobar|version:1|")
+
+(fact "compute-mac appends secret to end of (generate-mac-message ...) and computes SHA1 from that"
+ (compute-mac valid-params "secret" :mac) => "f9cc37a35218a85cb8871e9812311fd29e7e25b8"
+ (provided
+  (generate-mac-message valid-params :mac) => "base"))
+ 
+(fact "compute-hmac appends secret to end of (generate-mac-message ...) and computes HMAC-SHA1 from that"
+ (compute-hmac valid-params "secret" :mac) => "fd264415979bb17f68a3e4fd3b645b7e763e3b56"
+ (provided
+  (generate-mac-message valid-params :mac) => "base"))
+ 
+ 
+ 
