f26698da583fef64c507274054242f685e80daee
Juha Syrjälä <juha.syrjala@gmail.com>
2012-06-07 19:43:05 +0300

    Add maps from Maanmittauslaitos to map.html.

--- resources/public/js/map-display.js
+++ resources/public/js/map-display.js
@@ -1,14 +1,21 @@
 var map;
-function initMap() {
+
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var maanmittausUrl='http://tiles.kartat.kapsi.fi/peruskartta/{z}/{x}/{y}.jpg';
+
+var osmAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+var maanmittausAttribution = 'Maanmittauslaitos peruskartta';
+
+function initMap(mapCanvasId, url, attribution) {
     var startLocation = new L.LatLng(60.168564, 24.941111);
     // initialize the map on the "map" div
-    map = new L.Map('map');    
+    map = new L.Map(mapCanvasId);    
 
-    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    //var url = osmUrl;
     // create a CloudMade tile layer (or use other provider of your choice)
-    var mapTiles = new L.TileLayer(osmUrl, {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-	maxZoom: 18
+    var mapTiles = new L.TileLayer(url, {
+	'attribution': attribution,
+	'maxZoom': 18
     });
     
     // add the CloudMade layer to the map set the view to a given center and zoom
