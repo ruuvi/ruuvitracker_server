db2987d646f852cd7b64dfef0ed418004cd70f96
Juha Syrjälä <juha.syrjala@gmail.com>
2012-07-02 21:41:19 +0300

    Implements a consistent set of parsing functions.

--- src/ruuvi_server/parse.clj
+++ src/ruuvi_server/parse.clj
@@ -1,6 +1,27 @@
 (ns ruuvi-server.parse
   (:require [clojure.string :as string])
+  (:import [org.joda.time.format DateTimeFormat DateTimeFormatter]
+           [org.joda.time DateTime DateTimeZone])
+  (:import [java.lang IllegalArgumentException])
+  (:import [java.math BigDecimal])
+  (:import java.math.RoundingMode)
   )
+
+(defn- upper-matches-regex? [value regex]
+  (if value
+    (let [uppercase (.toUpperCase value)]
+      (if (re-matches regex uppercase)
+        true
+        false
+        ))
+    false))
+
+(def date-time-formatter (.withZone
+                          (DateTimeFormat/forPattern "YYYY-MM-dd'T'HH:mm:ss.SSSZ")
+                          (DateTimeZone/forID "UTC")
+  ))
+
+
 
 (defn parse-parameters
   "Params is map fieldname-keyword -> string.
@@ -41,4 +62,86 @@ or {field-name-keyword {:error 'Error message'}
 
 (defn get-error [parsed-values field]
   ((parsed-values field) :error))
+
+(defmacro parse [body error-msg]
+  `(try
+     ~body
+     (catch Exception e#
+       (throw (IllegalArgumentException. ~error-msg)))))
+
+;; parse functions
+(defn parse-decimal
+  "Parses string to BigDecimal instance. In case of errors, returns nil."
+  [value]
+  (parse (BigDecimal. value) "Expected decimal number."))
+
+(defn parse-unix-timestamp [value]
+  (parse (DateTime. (* 1000 (Long/valueOf value))
+                    (DateTimeZone/forID "UTC"))
+         "Expected Unix timestamp format."))
+
+(defn parse-date-time
+  "Parses string to DateTime instance. In case of errors, returns nil."
+  [date]
+  (parse
+   (.parseDateTime date-time-formatter date)
+   "Expected a timestamp in YYYY-MM-dd'T'HH:mm:ss.SSSZ (timezone required) format."
+   ))
+
+(defn parse-timestamp
+  "Parses a string to DateTime instance. 
+Supports unix timestamp and YYYY-MM-dd'T'HH:mm:ss.SSSZ"
+  [value]
+  (parse
+   (cond (not value) nil
+         (re-matches #"\d+" value) (parse-unix-timestamp value)
+         :default (parse-date-time value))
+   "Expected a timestamp in YYYY-MM-dd'T'HH:mm:ss.SSSZ (timezone required) or Unix timestamp format."
+   ))
+
+;; TODO check that seconds, minutes and degrees are in proper range [0,59]
+(defn nmea-latitude? [value]
+  (let [regex #"(\d*.?\d*),[NS]"]
+    (upper-matches-regex? value regex)))
+
+(defn nmea-longitude? [value]
+  (let [regex #"(\d*.?\d*),[EW]"]
+    (upper-matches-regex? value regex)))
+
+(defn- is-nmea-coordinate? [value]
+  (and value
+      (or (nmea-latitude? value)
+          (nmea-longitude? value))))
+
+(defn parse-nmea-coordinate [value]
+  (parse
+   (do
+     (when (not (is-nmea-coordinate? value))
+       (throw (IllegalArgumentException. (str value " is not valid NMEA coordinate"))))    
+     (let [upper (.toUpperCase value)
+           regex #"(\d*)(\d\d.?\d*),([NSWE])"
+           match-groups (re-matches regex upper)
+           area (match-groups 3)
+           sign (if (or (.contains area "S")
+                      (.contains area "W"))
+                  -1
+                  +1)
+           degrees (BigDecimal. (match-groups 1))
+           minutes (BigDecimal. (match-groups 2))
+           ]
+       (* sign (+ degrees (.divide minutes 60.0M 6 RoundingMode/FLOOR)))
+       ))
+   "Expected coordinate in NMEA format"
+   ))
+  
+(defn parse-integer [value]
+  (parse (Integer/valueOf value) "Expected an integer."))
+
+(defn parse-coordinate
+  "Parses string to a coordinate. String can be degrees decimal or NMEA format"
+  [value]
+  (parse 
+   (cond (is-nmea-coordinate? value) (parse-nmea-coordinate value)
+         :default (BigDecimal. value))
+   "Expected coordinate in NMEA or degrees decimal format."))
   
--- test/ruuvi_server/test/parse.clj
+++ test/ruuvi_server/test/parse.clj
@@ -1,6 +1,9 @@
 (ns ruuvi-server.test.parse
   (:use ruuvi-server.parse)
   (:use midje.sweet)
+  (:import [org.joda.time DateTime DateTimeUtils])
+  (:import [java.lang IllegalArgumentException])
+  (:import org.joda.time.DateTimeZone)
   )
 
 (def params {:a "value 1" :b "value 2" :c nil :d nil})
@@ -63,3 +66,106 @@
 (fact (get-value valid-values1 :a) => 32)
 
 (fact (get-error non-valid-values1 :a) => "foobar is not an integer")
+
+;; parse functions
+(fact (parse-integer "42") => 42)
+(fact (parse-integer "x") => (throws IllegalArgumentException) )
+
+
+;; nmea-latitude
+(fact "nmea-latitude? returns true for valid latitude string 5839.225,N"
+      (nmea-latitude? "5839.225,N") => true)
+
+(fact "nmea-latitude? returns true for valid latitude string 5839.225,s"
+      (nmea-latitude? "5839.225,s") => true)
+
+(fact "nmea-latitude? returns false for nil"
+      (nmea-latitude? nil) => false)
+
+(fact "nmea-latitude? returns false for 3.14"
+      (nmea-latitude? "3.14") => false)
+
+(fact "nmea-latitude? returns false for longitude string 5839.225,E"
+      (nmea-latitude? "5839.225,E") => false)
+
+(fact "nmea-latitude? returns false for longitude string 5839.225,W"
+      (nmea-latitude? "5839.225,W") => false)
+
+;; nmea-longitude?
+(fact "nmea-longitude? returns true for valid longitude string 5839.225,W"
+      (nmea-longitude? "5839.225,W") => true)
+
+(fact "nmea-longitude? returns true for valid longitude string 5839.225,e"
+      (nmea-longitude? "5839.225,e") => true)
+
+(fact "nmea-longitude? returns false for nil"
+      (nmea-longitude? nil) => false)
+
+(fact "nmea-longitude? returns false for 3.14"
+      (nmea-longitude? "3.14") => false)
+
+(fact "nmea-longitude? returns false for latitude string 5839.225,S"
+      (nmea-longitude? "5839.225,S") => false)
+
+(fact "nmea-longitude? returns false for latitude string 5839.225,N"
+      (nmea-longitude? "5839.225,N") => false)
+
+;; parse-nmea-coordinate
+(fact "parse-nmea-coordinate converts 5839.225,N to +58.653750"
+      (parse-nmea-coordinate "5839.225,N") => 58.653750M)
+
+(fact "parse-nmea-coordinate converts 5839.225,s to -58.653750"
+      (parse-nmea-coordinate "5839.225,s") => -58.653750M)
+
+(fact "parse-nmea-coordinate converts 839.225,E to +8.653750"
+      (parse-nmea-coordinate "839.225,E") => +8.653750M)
+
+(fact "parse-nmea-coordinate converts 839.225,W to -8.653750"
+      (parse-nmea-coordinate "839.225,W") => -8.653750M)
+
+(fact "parse-coordinate parses nil to nil"
+      (parse-coordinate nil)
+      => (throws IllegalArgumentException "Expected coordinate in NMEA or degrees decimal format."))
+
+(fact "parse-coordinate parses empty string to nil"
+      (parse-coordinate "")
+      => (throws IllegalArgumentException "Expected coordinate in NMEA or degrees decimal format."))
+
+(fact "parse-coordinate parses decimal string to BigDecimal"
+      (parse-coordinate "3.14") => 3.14M)
+
+(fact "parse-coordinate parses NMEA string to BigDecimal"
+      (parse-coordinate "839.225,W") => -8.653750M)
+
+;; decimal parsing
+(fact (parse-decimal "0.123") => 0.123M)
+
+(fact (parse-decimal nil) => (throws IllegalArgumentException "Expected decimal number."))
+
+(fact (parse-decimal "foobar") => (throws IllegalArgumentException "Expected decimal number.") )
+
+
+;; time and date parsing
+
+(def local-timezone (DateTimeZone/forID "Europe/Helsinki"))
+(def utc-timezone (DateTimeZone/forID "UTC"))
+(def local-date-time (DateTime. 2012, 3, 18, 23, 25, 9, 213, local-timezone))
+(def parsed-date-time (DateTime. 2012, 3, 18, 21, 25, 9, 213, utc-timezone))
+(def parsed-date-time-no-millis (DateTime. 2012, 3, 18, 21, 25, 9, 0, utc-timezone))
+(def date-time-text "2012-03-18T21:25:09.213+0000")
+
+(fact (parse-date-time date-time-text) => parsed-date-time)
+
+(fact (parse-date-time "foob") => (throws IllegalArgumentException "Expected a timestamp in YYYY-MM-dd'T'HH:mm:ss.SSSZ (timezone required) format."))
+
+(fact (parse-date-time nil) => (throws IllegalArgumentException "Expected a timestamp in YYYY-MM-dd'T'HH:mm:ss.SSSZ (timezone required) format."))
+
+(fact (parse-timestamp "non-timestamp-value") => (throws IllegalArgumentException "Expected a timestamp in YYYY-MM-dd'T'HH:mm:ss.SSSZ (timezone required) or Unix timestamp format."))
+
+(fact (parse-timestamp "1332105909") => parsed-date-time-no-millis)
+
+(fact (parse-timestamp date-time-text) => parsed-date-time) 
+
+(fact (parse-unix-timestamp "1332105909") => parsed-date-time-no-millis)
+
+(fact (parse-unix-timestamp "foo") => (throws IllegalArgumentException "Expected Unix timestamp format."))
