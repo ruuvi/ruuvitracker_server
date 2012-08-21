994a0a1f1a94bb497e5811290b565d21b01401a2
Juha Syrjälä <juha.syrjala@gmail.com>
2012-08-21 17:04:14 +0300

    Possible fix for race conditions in event_dao.clj.
    All event_dao functions that fetch and create if row is missing use now try-times macro. Fetch-create is attempted twice. This may still have a race condition. Fix #50.

--- src/ruuvi_server/util.clj
+++ src/ruuvi_server/util.clj
@@ -198,3 +198,25 @@ Supports unix timestamp and YYYY-MM-dd'T'HH:mm:ss.SSSZ"
       (catch Exception e
         (error e "Handling request " (str req) " failed")
         (throw e)))))
+
+
+(defn try-times*
+  "Executes thunk. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n thunk]
+  (loop [n n]
+    (if-let [result (try
+                      [(thunk)]
+                      (catch Exception e
+                        (when (zero? n)
+                          (throw e))))]
+      (result 0)
+      (recur (dec n)))))
+
+(defmacro try-times
+  "Executes body. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n & body]
+  `(try-times* ~n (fn [] ~@body)))
