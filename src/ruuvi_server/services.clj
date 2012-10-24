5c9416b7147687f1c3c2009f2dc39179dc591955
Juha Syrjälä <juha.syrjala@gmail.com>
2012-10-24 21:07:28 +0300

    nominatim search now actually works

--- src/ruuvi_server/services.clj
+++ src/ruuvi_server/services.clj
@@ -3,13 +3,12 @@
   (:require [clj-http.client :as client])
   )
 
-(def reverse-geocode
+(defn reverse-geocode
   "Reverse geo-coding (location -> address) using OpenStreetmap Nominatim service"
   [{:keys [lat lon]}]
   (let [nominatim-url "http://nominatim.openstreetmap.org/reverse"
-        query-params {:lat lat :lon lon :format :json}
-        response (client/get nominatim-url {{:query-params query-params} 
+        query-params {:lat lat :lon lon :format "json"}
+        response (client/get nominatim-url {:query-params query-params 
                                             :as :json})
         address (get-in response [:body :address])]
-    address
-    ))
+    address ))
