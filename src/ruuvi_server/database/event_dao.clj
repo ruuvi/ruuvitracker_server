ruuvi/ruuvitracker_server

commit 810f6ca81e19b49d61c5d5c98c24c7d5e669181d

Use clj-time. Fixes #62.

src/ruuvi_server/database/event_dao.clj
@@ -8,6 +8,8 @@
         [ruuvi-server.database.entities :only (tracker event event-session event-extension-type
                                                        event-extension-value event-location
                                                        event-annotation)]
+        [clj-time.core :only (date-time now)]
+        [clj-time.coerce :only (to-sql-date)]
         [clojure.tools.logging :only (debug info warn error)]
         )
   (:import [org.joda.time DateTime])
@@ -16,11 +18,11 @@
 ;; private functions
 (defn- to-sql-timestamp [^DateTime date]
   (if date
-    (java.sql.Timestamp. (.getMillis date))
+    (to-sql-date date)
     nil))
   
 (defn- current-sql-timestamp []
-  (to-sql-timestamp (DateTime.)))
+  (to-sql-timestamp (now)))
 
 ;; public functions
 (defn get-trackers [ids]
@@ -122,7 +124,7 @@
 (defn search-events 
   "Search events: criteria is a map that can contain following keys.
 - :storeTimeStart <DateTime>, find events that are created (stored) to database later than given time (inclusive).
-- :storeTimeDnd <DateTime>, find events that are created (stored) to database earlier than given time (inclusive).
+- :storeTimeEnd <DateTime>, find events that are created (stored) to database earlier than given time (inclusive).
 - :eventTimeStart <DateTime>, find events that are created in tracker later than given time (inclusive).
 - :eventTimeEnd <DateTime>, find events that are created in tracker earlier than given time (inclusive).
 - :maxResults <Integer>, maximum number of events. Default and maximum is 50.

project.clj
@@ -17,7 +17,7 @@
                  [amalloy/ring-gzip-middleware "0.1.2"]
 
                  [clj-json "0.5.0"]
-                 [aleph "0.3.0-beta7"]
+                 [aleph "0.3.0-beta8"]
                  
                  ;; logging
                  [org.clojure/tools.logging "0.2.4"
@@ -45,6 +45,7 @@
                  [com.cemerick/pomegranate "0.0.13"]
                  
                  ;; misc
+                 [clj-time "0.4.4"]
                  [joda-time/joda-time "2.1"]
                  [commons-codec/commons-codec "1.6"]
                  [org.clojure/tools.cli "0.2.2"]
@@ -58,7 +59,8 @@
                [midje "1.4.0" :exclusions [org.clojure/clojure]]
                ]}}
   ;; enable OTF (on-the-fly compilation)
-  :hooks [lein-otf.hooks]
+  ;; :hooks [lein-otf.hooks]
+  
   ;; Emit warnings on all reflection calls.
   ;;:warn-on-reflection true
   ;; AOT (ahead-of-time compilation) breaks migrations

src/ruuvi_server/client_api.clj
@@ -9,7 +9,6 @@
   (:use [clojure.tools.logging :only (debug info warn error)]
         [clojure.set :only (rename-keys)]
         )
-  (:import org.joda.time.DateTime)
   )
 
 (defn- select-extension-data

src/ruuvi_server/database/entities.clj
@@ -7,7 +7,6 @@
         [korma.core :only (defentity entity-fields table pk belongs-to has-many)]
         [clojure.tools.logging :only (debug info warn error)]
         )
-  (:import org.joda.time.DateTime)
   )
 
 (declare tracker)

src/ruuvi_server/parse.clj
@@ -1,10 +1,11 @@
 (ns ruuvi-server.parse
   (:require [clojure.string :as string])
-  (:import [org.joda.time.format DateTimeFormat DateTimeFormatter]
-           [org.joda.time DateTime DateTimeZone]
-           [java.lang IllegalArgumentException]
+  (:import [java.lang IllegalArgumentException]
            [java.math BigDecimal RoundingMode]
            )
+  (:use [clj-time.core :only (now time-zone-for-id ) ]
+        [clj-time.coerce :only (from-long) ]
+        [clj-time.format :only (formatter parse unparse) ])
   )
 
 (defn- upper-matches-regex? [^String value regex]
@@ -16,12 +17,9 @@
         ))
     false))
 
-(def date-time-formatter (.withZone
-                          (DateTimeFormat/forPattern "YYYY-MM-dd'T'HH:mm:ss.SSSZ")
-                          (DateTimeZone/forID "UTC")
-  ))
+(def date-time-formatter (formatter "YYYY-MM-dd'T'HH:mm:ss.SSSZ" (time-zone-for-id "UTC")))
 
-(defn timestamp [] (.print date-time-formatter (new org.joda.time.DateTime)))
+(defn timestamp [] (unparse date-time-formatter (now)))
 
 (defn parse-parameters
   "Params is map fieldname-keyword -> string.
@@ -63,7 +61,7 @@ or {field-name-keyword {:error 'Error message'}
 (defn get-error [parsed-values field]
   ((parsed-values field) :error))
 
-(defmacro parse [body error-msg]
+(defmacro parse-value [body error-msg]
   `(try
      ~body
      (catch Exception e#
@@ -73,18 +71,18 @@ or {field-name-keyword {:error 'Error message'}
 (defn parse-decimal
   "Parses string to BigDecimal instance. In case of errors, returns nil."
   [^String value]
-  (parse (BigDecimal. value) "Expected decimal number."))
+  (parse-value (BigDecimal. value) "Expected decimal number."))
 
+;; TODO add support for millisecs as decimal value (1232311.123)
 (defn parse-unix-timestamp [^String value]
-  (parse (DateTime. (* 1000 (Long/valueOf value))
-                    (DateTimeZone/forID "UTC"))
-         "Expected Unix timestamp format."))
+  (parse-value (from-long (* 1000 (Long/valueOf value)))
+               "Expected Unix timestamp format."))
 
 (defn parse-date-time
   "Parses string to DateTime instance. In case of errors, returns nil."
   [^String date]
-  (parse
-   (.parseDateTime date-time-formatter date)
+  (parse-value
+   (parse date-time-formatter date)
    "Expected a timestamp in YYYY-MM-dd'T'HH:mm:ss.SSSZ (timezone required) format."
    ))
 
@@ -92,7 +90,7 @@ or {field-name-keyword {:error 'Error message'}
   "Parses a string to DateTime instance. 
 Supports unix timestamp and YYYY-MM-dd'T'HH:mm:ss.SSSZ"
   [^String value]
-  (parse
+  (parse-value
    (cond (not value) nil
          (re-matches #"\d+" value) (parse-unix-timestamp value)
          :default (parse-date-time value))
@@ -114,7 +112,7 @@ Supports unix timestamp and YYYY-MM-dd'T'HH:mm:ss.SSSZ"
           (nmea-longitude? value))))
 
 (defn parse-nmea-coordinate [^String value]
-  (parse
+  (parse-value
    (do
      (when (not (is-nmea-coordinate? value))
        (throw (IllegalArgumentException. (str value " is not valid NMEA coordinate"))))    
@@ -135,12 +133,12 @@ Supports unix timestamp and YYYY-MM-dd'T'HH:mm:ss.SSSZ"
    ))
   
 (defn parse-integer [value]
-  (parse (Integer/valueOf value) "Expected an integer."))
+  (parse-value (Integer/valueOf value) "Expected an integer."))
 
 (defn parse-coordinate
   "Parses string to a coordinate. String can be degrees decimal or NMEA format"
   [^String value]
-  (parse 
+  (parse-value 
    (cond (is-nmea-coordinate? value) (parse-nmea-coordinate value)
          :default (BigDecimal. value))
    "Expected coordinate in NMEA or degrees decimal format."))

src/ruuvi_server/util.clj
@@ -1,14 +1,14 @@
 (ns ruuvi-server.util
-    (:import [org.joda.time.format DateTimeFormat DateTimeFormatter]
-             [org.joda.time DateTime DateTimeZone]
-             [java.lang IllegalArgumentException]
+    (:import [java.lang IllegalArgumentException]
              [java.math BigDecimal RoundingMode]
              )
     (:require [clojure.walk :as walk]
               [clj-json.core :as json]
               [ruuvi-server.parse :as parse]
               )
-    (:use [clojure.tools.logging :only (debug info warn error)])
+    (:use [clojure.tools.logging :only (debug info warn error)]
+          [clj-time.coerce :only (from-date) ]
+          )
     )
 
 (defn modify-map 
@@ -83,7 +83,7 @@
   "Convert objects in map to strings, assumes that map is flat"
   [data-map]
   (walk/prewalk (fn [item]
-                  (cond (instance? java.util.Date item) (.print parse/date-time-formatter (DateTime. item))
+                  (cond (instance? java.util.Date item) (.print parse/date-time-formatter (from-date item))
                         (instance? java.math.BigDecimal item) (str item)
                         :else item)
                   ) data-map))

test/ruuvi_server/test/parse.clj
@@ -1,8 +1,8 @@
 (ns ruuvi-server.test.parse
-  (:use midje.sweet
+  (:use [midje.sweet :only (fact throws)]
+        [clj-time.core :only (date-time time-zone-for-id to-time-zone)]
         ruuvi-server.parse)
-  (:import [org.joda.time DateTime DateTimeUtils DateTimeZone]
-           [java.lang IllegalArgumentException])
+  (:import [java.lang IllegalArgumentException])
   )
 
 (def params {:a "value 1" :b "value 2" :c nil :d nil})
@@ -146,11 +146,11 @@
 
 ;; time and date parsing
 
-(def local-timezone (DateTimeZone/forID "Europe/Helsinki"))
-(def utc-timezone (DateTimeZone/forID "UTC"))
-(def local-date-time (DateTime. 2012, 3, 18, 23, 25, 9, 213, local-timezone))
-(def parsed-date-time (DateTime. 2012, 3, 18, 21, 25, 9, 213, utc-timezone))
-(def parsed-date-time-no-millis (DateTime. 2012, 3, 18, 21, 25, 9, 0, utc-timezone))
+(def local-timezone (time-zone-for-id "Europe/Helsinki"))
+(def utc-timezone (time-zone-for-id "UTC"))
+(def local-date-time (to-time-zone (date-time 2012, 3, 18, 23, 25, 9, 213) local-timezone))
+(def parsed-date-time (to-time-zone (date-time 2012, 3, 18, 21, 25, 9, 213) utc-timezone))
+(def parsed-date-time-no-millis (to-time-zone (date-time 2012, 3, 18, 21, 25, 9, 0) utc-timezone))
 (def date-time-text "2012-03-18T21:25:09.213+0000")
 
 (fact (parse-date-time date-time-text) => parsed-date-time)

test/ruuvi_server/test/util.clj
@@ -1,9 +1,7 @@
 (ns ruuvi-server.test.util
-  (:import [org.joda.time DateTime DateTimeUtils])
   (:use ruuvi-server.util)
   (:use midje.sweet)
   (:require [clojure.java.io :as io])
-  (:import org.joda.time.DateTimeZone)
   )
 
 ;; map handling

commit 189f3ab4477fe489812dd688b998a2aa405ae0b3

Fix date handling bug introduced in 810f6ca81e19b49d61c5d5c98c24c7d5e669181d

src/ruuvi_server/database/event_dao.clj
@@ -9,20 +9,15 @@
                                                        event-extension-value event-location
                                                        event-annotation)]
         [clj-time.core :only (date-time now)]
-        [clj-time.coerce :only (to-sql-date)]
+        [clj-time.coerce :only (to-timestamp)]
         [clojure.tools.logging :only (debug info warn error)]
         )
   (:import [org.joda.time DateTime])
   )
 
 ;; private functions
-(defn- to-sql-timestamp [^DateTime date]
-  (if date
-    (to-sql-date date)
-    nil))
-  
 (defn- current-sql-timestamp []
-  (to-sql-timestamp (now)))
+  (to-timestamp (now)))
 
 ;; public functions
 (defn get-trackers [ids]
@@ -133,10 +128,10 @@ TODO make maxResults default configurable.
 "
   [criteria]
 
-  (let [event-start (to-sql-timestamp (:eventTimeStart criteria))
-        event-end (to-sql-timestamp (:eventTimeEnd criteria))
-        store-start (to-sql-timestamp (:storeTimeStart criteria))
-        store-end (to-sql-timestamp (:storeTimeEnd criteria))
+  (let [event-start (to-timestamp (:eventTimeStart criteria))
+        event-end (to-timestamp (:eventTimeEnd criteria))
+        store-start (to-timestamp (:storeTimeStart criteria))
+        store-end (to-timestamp (:storeTimeEnd criteria))
         tracker-ids (:trackerIds criteria)
         session-ids (:sessionIds criteria)
         max-result-count (:max-search-results (:client-api (conf/get-config)))
@@ -186,7 +181,7 @@ TODO make maxResults default configurable.
 
 (defn create-event [data]
   (transaction
-   (let [event-time (or (to-sql-timestamp (:event_time data))
+   (let [event-time (or (to-timestamp (:event_time data))
                         (current-sql-timestamp))
          tracker (get-tracker-by-code! (:tracker_code data))
          ;; TODO trim session_code to length of db field