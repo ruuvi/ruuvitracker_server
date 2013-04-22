92d7307681787a267cfcc7823e4384a94afa88a1
Juha Syrjälä <juha.syrjala@gmail.com>
2013-04-23 00:51:29 +0300

    Upgraded deps.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "RuuviTracker server"
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.7.5"]
-            [lein-midje "2.0.4"]
+            [lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]
             [org.clojars.llasram/lein-otf "2.1.2"]
             ]
@@ -15,7 +15,7 @@
                  [ring/ring-json "0.1.2"]
                  [amalloy/ring-gzip-middleware "0.1.2"]
 
-                 [cheshire "5.0.1"]
+                 [cheshire "5.1.1"]
                  [aleph "0.3.0-beta11"]
                  
                  ;; logging
@@ -25,26 +25,26 @@
                    commons-logging/commons-logging
                    org.slf4j/slf4j-api
                    org.slf4j/slf4j-log4j12]]
-                 [ch.qos.logback/logback-classic "1.0.9"]
+                 [ch.qos.logback/logback-classic "1.0.11"]
                  [org.slf4j/log4j-over-slf4j "1.7.2"]
 
                  ;; external services
-                 [clj-http "0.6.3"]
+                 [clj-http "0.7.2"]
                  
                  ;; database
                  [postgresql/postgresql "9.1-901.jdbc4"]
-                 [com.h2database/h2 "1.3.168"]
+                 [com.h2database/h2 "1.3.171"]
                  [com.jolbox/bonecp "0.7.1.RELEASE"]
-                 [korma "0.3.0-beta15" :exclusions [log4j/log4j]]
+                 [korma "0.3.0-RC5" :exclusions [log4j/log4j]]
                  [lobos "1.0.0-beta1"]
                  [org.clojure/core.cache "0.6.2"]
 
                  ;; remote repl
-                 [org.clojure/tools.nrepl "0.2.0"]
+                 [org.clojure/tools.nrepl "0.2.2"]
                  ;;[com.cemerick/pomegranate "0.0.13"]
                  
                  ;; misc
-                 [clj-time "0.4.4"]
+                 [clj-time "0.5.0"]
                  [commons-codec/commons-codec "1.6"]
                  [org.clojure/tools.cli "0.2.2"]
                  ;; TODO lein 0.2.0-RC2 requires this
@@ -57,7 +57,7 @@
   :profiles {:dev
              {:dependencies
               [
-               [midje "1.5.0" :exclusions [org.clojure/clojure]]
+               [midje "1.5.1" :exclusions [org.clojure/clojure]]
                ]}}
 
   :jvm-opts ["-server" "-XX:+UseConcMarkSweepGC"]
@@ -67,7 +67,9 @@
   
   ;; Emit warnings on all reflection calls.
   ;;:warn-on-reflection true
-  ;; AOT (ahead-of-time compilation) breaks migrations
+
+  ;; AOT (ahead-of-time compilation). Breaks migrations and midje tests
   ;; :aot [ruuvi-server.launcher]
-  :main ^:skip-aot ruuvi-server.launcher
+
+  :main ruuvi-server.launcher
   )
